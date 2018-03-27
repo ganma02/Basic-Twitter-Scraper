4f93b5ea8f275f9af907deda4b1364a45d02b6ee
Anas Amir <anasamir6@gmail.com>
2018-03-27 18:34:51 +0800

    Add files via upload

---
+++ scrape_twitter.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 27 11:37:22 2018
+Twitter Scraper
+@author: win_8
+"""
+
+import requests
+import bs4
+
+class TwitterUser():
+    
+      def __init__(self, username):
+          self.username = username
+            
+      # Getting twitter name by using username
+      def get_twitter_name(self):
+          res = requests.get('https://twitter.com/{}'.format(self.username))
+          #res.raise_for_status()
+      
+          twitterSoup = bs4.BeautifulSoup(res.text, 'lxml')
+          a_search = twitterSoup.select('a')
+          
+          # Finding the index of 'a' that has the name
+          for a,b in enumerate(a_search):
+                if a_search[a].get('href') == '/'+self.username:
+                      name = a_search[a].getText()
+                      name = name.strip()
+                      break
+          return name
+      
+      # Getting twitter bio by using username
+      def get_twitter_bio(self):
+          res = requests.get('https://twitter.com/{}'.format(self.username))
+          #res.raise_for_status()
+      
+          twitterSoup = bs4.BeautifulSoup(res.text, 'lxml')
+          p_search = twitterSoup.select('p')
+          bio = p_search[0].getText()
+          return bio
+      
+      
+      # Getting amount of followers in twitter by using username
+      def get_twitter_follower(self):
+          res = requests.get('https://twitter.com/{}'.format(self.username))
+          #res.raise_for_status()
+      
+          twitterSoup = bs4.BeautifulSoup(res.text, 'lxml')
+          span_search = twitterSoup.select('span')
+          
+          # Finding the index of 'span' that has the followers count
+          for a,b in enumerate(span_search):
+                if span_search[a].getText() == 'Pengikut': #'Pengikut' in Malay means 'Follower'. This is because my ISP used Malay language.
+                      follower = span_search[a + 2].getText()
+                      break
+          
+          return follower
+
+if __name__ == '__main__':
+    #user = TwitterUser('testuser')
+    #print('My name is ' + user.get_twitter_name())
+    #print(user.get_twitter_bio())
+    #print('I have ' + user.get_twitter_follower() + ' followers.')
---
+++ use_username.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 27 13:32:44 2018
+Using the Twitter Scraper
+@author: win_8
+"""
+
+import pandas as pd
+import scrape_twitter as st
+
+usernames = pd.read_csv('Twitter Username Sample.csv', header = None).iloc[:,0]
+for i in usernames:
+      user = st.TwitterUser(i)
+      print('My name is ' + user.get_twitter_name())
+      print(user.get_twitter_bio())
+      print('I have ' + user.get_twitter_follower() + ' followers.')
